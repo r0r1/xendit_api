d0de8a0b4d3a57221c60e8273308078876d62a00
Rori Kurniadi <rori.development@gmail.com>
2022-04-22 01:17:40 +0700

    add required callback url

--- lib/xendit_api/client.rb
+++ lib/xendit_api/client.rb
@@ -9,6 +9,7 @@ require 'xendit_api/api/qr_code'
 require 'xendit_api/api/v2/invoice'
 require 'xendit_api/api/v2/account'
 require 'xendit_api/api/fee_rule'
+require 'xendit_api/api/callback_url'
 
 
 module XenditApi
