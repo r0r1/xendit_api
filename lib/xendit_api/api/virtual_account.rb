182487e1c838dc6db88e0cd143480f9cedeee026
Rori Kurniadi <rori.development@gmail.com>
2022-05-18 17:23:24 +0700

    add headers

--- lib/xendit_api/api/virtual_account.rb
+++ lib/xendit_api/api/virtual_account.rb
@@ -35,9 +35,9 @@ module XenditApi
         XenditApi::Model::VirtualAccount.new(virtual_account_response)
       end
 
-      def find(id, params = nil)
+      def find(id, params = nil, headers = nil)
         find_path = "#{PATH}/#{id}"
-        response = client.get(find_path, params)
+        response = client.get(find_path, params, headers)
         virtual_account_params = permitted_virtual_account_params(response)
         XenditApi::Model::VirtualAccount.new(virtual_account_params)
       end
